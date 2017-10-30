8a202afd3782d92bbc8f0047de9d59a753d8966e
sasankadilshan <sasanka.dilshan@peakadventuretravel.com>
2017-10-30 20:46:57 +0530

    addding failure test

--- Integration/Workspace2/maven-tutorial/src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ Integration/Workspace2/maven-tutorial/src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -3,12 +3,20 @@ package com.mulesoft.training;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.mule.api.MuleEvent;
 import org.mule.tck.junit4.FunctionalTestCase;
 
 public class HelloMavenTest extends FunctionalTestCase {
 
     public void mavenFlowReturnsHelloMaven() throws Exception {
         runFlowAndExpect("maven-tutorial", "Hello");
+    }
+    
+    @Test
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception {
+      MuleEvent event = runFlow("retrieve-flights");
+      String contentType = event.getMessage().getOutboundProperty("Content-Type");
+      assertEquals("application/json", contentType);
     }
     
     @Override
